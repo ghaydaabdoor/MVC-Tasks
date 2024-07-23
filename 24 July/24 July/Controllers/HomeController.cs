89e25ec2d7f9bdc8ac95acd30bc2bc0873eb5faa
ghaydaabdoor <ghaydaa.bdoor@outlook.com>
2024-07-24 02:00:52 +0300

    second task

---
+++ 24 July/24 July/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _24_July.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Info()
+        {
+            ViewBag.Message = "Feedback details";
+            string fname = Request["fname"];
+            string lname= Request["lname"];
+            string email= Request["email"];
+            string phone= Request["phone"];
+            string gender = Request["gender"];
+            string subject = Request["subject"];
+            ViewBag.FName = fname;
+            ViewBag.LName = lname;
+            ViewBag.Email = email;
+            ViewBag.Phone = phone;
+            ViewBag.Gender = gender;
+            ViewBag.Subject = subject;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ 24 July/24 July/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24_July.MvcApplication" Language="C#" %>
